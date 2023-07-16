d94ac502059f14db4c18a62b315989802082a3ac
Dmytry Bondariev <bondarievmd@gmail.com>
2023-07-16 20:55:46 +0300

    Initialized User app with abstract user model

---
+++ user/models.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(_("email address"), unique=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
