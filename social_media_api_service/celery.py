1a3e6c0b8670a9973fbffe3147da883bc7fed239
Dmytry Bondariev <bondarievmd@gmail.com>
2023-07-19 15:57:07 +0300

    Last changes trying to make Post scheduling work

--- social_media_api_service/celery.py
+++ social_media_api_service/celery.py
@@ -4,7 +4,7 @@ from celery import Celery
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "social_media_api_service.settings")
 
-app = Celery("SocialMediaAPI")
+app = Celery("social_media_api_service")
 
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
