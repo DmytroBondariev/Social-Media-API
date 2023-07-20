15256e311dd5e876a88727b355d80013787e9b5a
Dmytry Bondariev <bondarievmd@gmail.com>
2023-07-20 22:58:10 +0300

    Fixed permissions

--- social_media/permissions.py
+++ social_media/permissions.py
@@ -3,15 +3,9 @@ from rest_framework.permissions import BasePermission
 
 class IsProfileOwner(BasePermission):
     def has_object_permission(self, request, view, obj):
-        if request.method == "GET":
-            return True
-
-        return obj.user == request.user
+        return request.method == "GET" or obj.user == request.user
 
 
 class IsPostOwner(BasePermission):
     def has_object_permission(self, request, view, obj):
-        if request.method == "GET":
-            return True
-
-        return obj.author == request.user.profile
+        return request.method == "GET" or obj.author == request.user.profile
